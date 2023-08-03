7d128063cc6591efd23e8333e9a37e8401028b8c
Ollie Pugh <oliver.pugh@mac.com>
2023-08-03 22:02:13 +0100

    add speeding violations

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import {
   RwgGame,
   CONTROL_TYPE,
   ControlEvent,
+  Match,
+  ELEMENT_TYPE,
 } from "@OlliePugh/rwg-game";
 import { Server as SocketServer } from "socket.io";
 
@@ -18,11 +20,103 @@ const serialport = new SerialPort({
   baudRate: 115200,
 });
 
-serialport.on("data", (data) => {
-  console.log("Received serial data:", data.toString());
+interface Cooldown {
+  currentTimeout?: ReturnType<typeof setTimeout>;
+  lastRecordedSpeed?: number;
+}
+
+const SLOW_DOWN_LENGTH = 3; // seconds
+const SPEED_BAN_SPEED = 0;
+
+let receivedData = Buffer.alloc(0); // Buffer to store incoming data
+const speedBans: Cooldown[] = [{}, {}];
+let currentMatch: Match;
+
+serialport.on("data", (data: any) => {
+  // Concatenate the received data with the existing buffer
+  receivedData = Buffer.concat([receivedData, data]);
+  // Check if the end byte (0x0d) is present in the received data
+  const endByteIndex = receivedData.indexOf(0x0d);
+
+  // If the end byte is found, process the complete message
+  if (endByteIndex !== -1) {
+    const completeMessage = receivedData.slice(0, endByteIndex + 1);
+    processMessage(completeMessage);
+
+    // Remove the processed message from the buffer
+    receivedData = receivedData.slice(endByteIndex + 1);
+  }
 });
 
-serialport.on("error", (err) => {
+const speedViolationCleared = (playerIndex: number) => {
+  delete speedBans[playerIndex].currentTimeout;
+  if (speedBans[playerIndex].lastRecordedSpeed != null) {
+    writeSpeed(speedBans[playerIndex].lastRecordedSpeed!, playerIndex == 0);
+  }
+};
+
+const commitSpeedViolation = (playerIndex: number) => {
+  emitSpeedViolationMessage(SLOW_DOWN_LENGTH, playerIndex);
+  writeSpeed(SPEED_BAN_SPEED, playerIndex == 0);
+};
+
+const emitSpeedViolationMessage = (
+  secondsRemaining: number,
+  playerIndex: number
+) => {
+  let message =
+    secondsRemaining === 0
+      ? ""
+      : `VIRTUAL SPIN OFF!\nRegain controls in ${secondsRemaining}`;
+
+  currentMatch.getPlayers()[playerIndex].updateUserInterface({
+    "speed-violation-text": {
+      extraData: {
+        message,
+      },
+    },
+  });
+
+  if (secondsRemaining-- > 0) {
+    speedBans[playerIndex].currentTimeout = setTimeout(() => {
+      emitSpeedViolationMessage(secondsRemaining, playerIndex);
+    }, 1000);
+  }
+
+  if (secondsRemaining == -1) {
+    // the penalty has been cleared
+    speedViolationCleared(playerIndex);
+  }
+};
+
+const processMessage = (message: Uint8Array) => {
+  const isPlayer1 = message[0] % 2 == 1;
+  const playerIndex = isPlayer1 ? 0 : 1;
+  const event = message[0] - playerIndex;
+  switch (event) {
+    case SERIAL_EVENT_KEYS.SPEED_VIOLATION:
+      if (speedBans[playerIndex].currentTimeout == undefined) {
+        commitSpeedViolation(playerIndex);
+      }
+
+      console.log(`speed violation for player ${isPlayer1 ? 1 : 2}`);
+      break;
+
+    case SERIAL_EVENT_KEYS.CAR_DISMOUNT:
+      // Handle CAR_DISMOUNT message
+      break;
+
+    case SERIAL_EVENT_KEYS.LAP_COMPLETE:
+      console.log("lap complete");
+      break;
+
+    default:
+      console.log(`Unknown event key ${message[0]}`);
+      break;
+  }
+};
+
+serialport.on("error", (err: any) => {
   console.error("Serial port error:", err.message);
 });
 
@@ -32,6 +126,9 @@ serialport.on("open", () => {
 
 const SERIAL_EVENT_KEYS = {
   CONTROL: 0x01,
+  SPEED_VIOLATION: 0x03,
+  CAR_DISMOUNT: 0x05,
+  LAP_COMPLETE: 0x07,
   FINISH_MESSAGE: 0x0d,
 };
 
@@ -67,12 +164,24 @@ const gameConfig: RwgConfig = {
           inputMap: [],
         },
       ],
+      rateLimit: 10,
       position: {
         x: 0.5,
         y: 0.8,
       },
       displayOnDesktop: true,
       size: 0.5,
+    },
+    {
+      id: "speed-violation-text",
+      type: ELEMENT_TYPE.text,
+      position: {
+        x: 0.1,
+        y: 0.1,
+      },
+      displayOnDesktop: true,
+      size: 1,
+      extraData: { message: "" },
     },
   ],
   countdownSeconds: 0,
@@ -88,25 +197,29 @@ const gameConfig: RwgConfig = {
         port: 8004,
       },
     },
-    {
-      id: "car2",
-      onControl: (eventData) => {
-        onControl(eventData, false);
-      },
-      stream: {
-        address: "https://stream.ollieq.co.uk/janus",
-        id: 2,
-        port: 8005,
-      },
-    },
+    // {
+    //   id: "car2",
+    //   onControl: (eventData) => {
+    //     onControl(eventData, false);
+    //   },
+    //   stream: {
+    //     address: "https://stream.ollieq.co.uk/janus",
+    //     id: 2,
+    //     port: 8005,
+    //   },
+    // },
   ],
 };
 
 const gameServer = new RwgGame(gameConfig, httpServer, app, io);
-gameServer.on(RWG_EVENT.MATCH_STATE_CHANGE, (newState) => {
+
+gameServer.on(RWG_EVENT.MATCH_STATE_CHANGE, (newState, match) => {
+  if (newState === MATCH_STATE.COUNTDOWN) {
+    currentMatch = match;
+  }
   if (
-    newState == MATCH_STATE.COMPLETED ||
-    newState == MATCH_STATE.WAITING_FOR_PLAYERS
+    newState === MATCH_STATE.COMPLETED ||
+    newState === MATCH_STATE.WAITING_FOR_PLAYERS
   ) {
     const buffer = Buffer.alloc(6); // stop the cars
     buffer[0] = SERIAL_EVENT_KEYS.CONTROL;
@@ -119,6 +232,14 @@ gameServer.on(RWG_EVENT.MATCH_STATE_CHANGE, (newState) => {
   }
 });
 
+const writeSpeed = (value: number, isPlayer1: boolean) => {
+  const buffer = Buffer.alloc(3);
+  buffer[0] = SERIAL_EVENT_KEYS.CONTROL + +!isPlayer1; // add one if not player1
+  buffer[1] = new Uint8Array([value])[0];
+  buffer[2] = SERIAL_EVENT_KEYS.FINISH_MESSAGE;
+  serialport.write(buffer);
+};
+
 const onControl = async (
   controlEvent: ControlEvent | ControlEvent[],
   isPlayer1: boolean
@@ -126,15 +247,15 @@ const onControl = async (
   const controls: ControlEvent[] = Array.isArray(controlEvent)
     ? controlEvent
     : [controlEvent];
+  const playerIndex = isPlayer1 ? 0 : 1;
 
   controls.forEach((control) => {
     if (control.controlName == "control-slider") {
-      const buffer = Buffer.alloc(3);
-      buffer[0] = SERIAL_EVENT_KEYS.CONTROL + +!isPlayer1; // add one if not player1
-      buffer[1] = new Uint8Array([control.value])[0];
-      buffer[2] = SERIAL_EVENT_KEYS.FINISH_MESSAGE;
-
-      serialport.write(buffer);
+      speedBans[isPlayer1 ? 0 : 1].lastRecordedSpeed = control.value; // save this incase this exceeds the threshold
+      if (speedBans[playerIndex].currentTimeout == null) {
+        // write if the player is not currently speed banned
+        writeSpeed(control.value, isPlayer1);
+      }
     }
   });
 };
